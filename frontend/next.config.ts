de234121f2b917734328153d59918a05c2b3fcc7
MohitUnecha <mohitkunecha@gmail.com>
2026-02-04 14:27:34 -0500

    Remove basePath for custom domain

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,7 +5,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/Mohit-Unecha-s-Portfolio-Website' : '',
 };
 
 export default nextConfig;
